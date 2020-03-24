7704f60b64f2bd4311d5dbe06e52d953bfaa1df3
KyloRen3600 <kyloren3600@gmail.com>
2020-03-24 10:22:41 +0100

    - Menu principal TER-MI-NE

--- indev/menu.py
+++ indev/menu.py
@@ -1,26 +1,36 @@
 from tkinter import *
 
-#Nouvelle partie
-#Charger une partie
-fen = Tk()
-fen.overrideredirect(True)
+root = Tk()
+yDefilB = Scrollbar(root, orient='vertical')
+yDefilB.grid(row=0, column=1, sticky='ns')
 
-positionRight = int(fen.winfo_screenwidth() / 3 - fen.winfo_reqwidth() / 3)
-positionDown = int(fen.winfo_screenheight() / 3 - fen.winfo_reqheight() / 2)
-fen.geometry("+{}+{}".format(positionRight, positionDown))
+xDefilB = Scrollbar(root, orient='horizontal')
+xDefilB.grid(row=1, column=0, sticky='ew')
 
-
-background = PhotoImage(file="background.png")
-
-menu = Canvas(fen, border=-5, background="black", width=background.width(), height=background.height())
-menu.create_image(0, 0, image=background, anchor=NW)
-menu.pack(fill=BOTH)
-
-button1 = Button(fen, text = "Quit", command = fen.quit, anchor = W)
-button1.configure(width = 10, activebackground = "#33B5E5", relief = FLAT)
-button1_window = menu.create_window(10, 10, anchor=NW, window=button1)
+listSel = Listbox(root,
+     xscrollcommand=xDefilB.set,
+     yscrollcommand=yDefilB.set)
+listSel.grid(row=0, column=0, sticky='nsew')
 
 
-fen.after(15000, menu.destroy)
-#fen.after(5000, lambda: fen.overrideredirect(False))
-fen.mainloop()
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "a")
+listSel.insert(END, "b")
+root.mainloop()
--- indev/test.py
+++ indev/test.py
@@ -1,19 +1,19 @@
-import tkinter as Tkinter
-
+import os
 from tkinter import *
+from tkinter import filedialog
 
-class NoBorderWindow(Tk):
+class Fenetre(Tk): #L'idée et de créer une fenêtre sans brodures mais qui apparaissent tout de même dans la barre de tâches
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		self.little = Tk()
-		self.little.geometry("0x0")
-		self.little.lift(aboveThis=self)
-		self.little.bind("<Map>", self.apparition)
-		self.little.bind("<Unmap>", self.disparition)
-		self.little.protocol("WM_DELETE_WINDOW", self.quit)
-		self.overrideredirect(True)
-		self.lift()
-		self.attributes('-topmost', True)
+		self.little = Tk() #On crée une 2nd fenêtre qui n'est là que comme "bouton" dans la barre de tâches
+		self.little.geometry("0x0") #On minimise sa taille
+		self.little.lift(aboveThis=self) #On la positionne derrière la fenêtre principale
+		self.little.bind("<Map>", self.apparition) #Lors de son apparition on fait appraître la fenêtre principale
+		self.little.bind("<Unmap>", self.disparition) #Même chose pour la disparition
+		self.little.protocol("WM_DELETE_WINDOW", self.quit) #Lors de sa fermeture on ferme la fenêtre principale
+		self.overrideredirect(True) #On enlève les bordures de la fenêtre principale
+		self.lift() #On la fait passer au premier plan
+		self.attributes('-topmost', True) #On ajoute un attribut pour toujours la laisser au premier plan
 
 		self.background_image = PhotoImage(file="background.png")
 		self.canvas = Canvas(self, border=-5, background="black", width=self.background_image.width(), height=self.background_image.height())
@@ -24,9 +24,6 @@ class NoBorderWindow(Tk):
 
 		self.barre_outils = Frame(self, background="#292626")
 		self.barre_outils.grid(row=0, column=0,)
-
-		#self.bouton_reduire = Button(self.barre_outils, bg="#960d03", text="-", fg="white", border=0, relief=SUNKEN, command=self.little.iconify)
-		#self.bouton_reduire.grid(row=0, column=0)
 
 		self.bouton_reduire = Button(self.barre_outils, width=3, height=1, bg="#292626", activebackground="#1f1c1c", text="-", font="Helvetica 10 bold", fg="white", activeforeground="white", border=0, relief=SUNKEN, command=self.little.iconify)
 		self.bouton_reduire.grid(row=0, column=1)
@@ -42,18 +39,22 @@ class NoBorderWindow(Tk):
 		centrer_fenetre(self.little)
 		centrer_fenetre(self)
 
-		font="Helvetica 18"
-		width=15
+		font = "Helvetica 18"
+		width = 15
 		self.bouton_bg = "#1f1e1d"
 		self.bouton_fg = "#e0deda"
-		self.bouton_activebg = "#737270"
+		self.bouton_activebg = "#292826"
 		self.bouton_activefg = "#737270"
-		self.bouton_nouvelle = Button(self, text="Nouvelle partie", bg=self.bouton_bg, fg=self.bouton_fg, activebackground=self.bouton_activebg, activeforeground=self.bouton_activefg, width=width, font=font, command=lambda: can.config(bg="green"), border=0, relief=SUNKEN)
-		self.bouton_charger = Button(self, text="Charger une partie", bg=self.bouton_bg, fg=self.bouton_fg, activebackground=self.bouton_activebg, width=width, font=font, command=lambda: can.config(bg="green"), border=0, relief=SUNKEN)
-		self.bouton_quitter = Button(self, text="Quitter", bg=self.bouton_bg, fg=self.bouton_fg, activebackground=self.bouton_activebg, width=width, font=font, command=self.little.quit, border=0, relief=SUNKEN)
-		self.nouvelle = self.canvas.create_window(self.canvas.winfo_width()//2, self.canvas.winfo_height()//2-self.canvas.winfo_height()//5, window=self.bouton_nouvelle, anchor=CENTER)
-		self.charger = self.canvas.create_window(self.canvas.winfo_width()//2, self.canvas.winfo_height()//2, window=self.bouton_charger, anchor=CENTER)
-		self.quitter = self.canvas.create_window(self.canvas.winfo_width()//2, self.canvas.winfo_height()//2+self.canvas.winfo_height()//5, window=self.bouton_quitter, anchor=CENTER)
+		self.boutons = []
+		self.boutons.append(Button(self, text="Nouvelle Partie", bg=self.bouton_bg, fg=self.bouton_fg, activebackground=self.bouton_activebg, activeforeground=self.bouton_activefg, width=width, font=font, command=self.ouvrir_nouvelle, border=0, relief=SUNKEN))
+		self.boutons.append(Button(self, text="Charger une Partie", bg=self.bouton_bg, fg=self.bouton_fg, activebackground=self.bouton_activebg, activeforeground=self.bouton_activefg, width=width, font=font, command=self.ouvrir_charger, border=0, relief=SUNKEN))
+		self.boutons.append(Button(self, text="Quitter", bg=self.bouton_bg, fg=self.bouton_fg, activebackground=self.bouton_activebg, activeforeground=self.bouton_activefg, width=width, font=font, command=self.little.quit, border=0, relief=SUNKEN))
+		for bouton in self.boutons:
+			bouton.bind("<Enter>", self.entree_bouton)
+			bouton.bind("<Leave>", self.sortie_bouton)
+		self.b1 = self.canvas.create_window(self.canvas.winfo_width()//2, self.canvas.winfo_height()//2-self.canvas.winfo_height()//5, window=self.boutons[0], anchor=CENTER)
+		self.b2 = self.canvas.create_window(self.canvas.winfo_width()//2, self.canvas.winfo_height()//2, window=self.boutons[1], anchor=CENTER)
+		self.b3 = self.canvas.create_window(self.canvas.winfo_width()//2, self.canvas.winfo_height()//2+self.canvas.winfo_height()//5, window=self.boutons[2], anchor=CENTER)
 
 	def apparition(self, e):
 		self.wm_deiconify()
@@ -62,14 +63,36 @@ class NoBorderWindow(Tk):
 		self.wm_withdraw()
 		self.lift()
 
+	def ouvrir_accueil(self):
+		self.boutons[0]["text"] = "Nouvelle Partie"
+		self.boutons[1]["text"] = "Charger une Partie"
+		self.boutons[2]["text"] = "Quitter"
+		self.boutons[0]["command"] = self.ouvrir_nouvelle
+		self.boutons[1]["command"] = self.ouvrir_charger
+		self.boutons[2]["command"] = self.little.quit
+
+	def ouvrir_nouvelle(self):
+		self.boutons[0]["text"] = "Partie Normale"
+		self.boutons[1]["text"] = "Partie Blitz"
+		self.boutons[2]["text"] = "Retour"
+		self.boutons[0]["command"] = lambda: nouvelle_partie(self._root(), "normale")
+		self.boutons[1]["command"] = lambda: nouvelle_partie(self._root(), "blizt")
+		self.boutons[2]["command"] = self.ouvrir_accueil
+
+	def ouvrir_charger(self):
+		charger_partie(self, filedialog.askopenfilename(initialdir=os.getcwd()+"/parties", title="Choisissez une partie",
+								   filetypes=[("Sauvegarde de partie", "*.save")]))
+
 	def entree_bouton(self, e):
 		e.widget["bg"] = e.widget["activebackground"]
 	def sortie_bouton(self, e):
 		color = None
 		if e.widget == self.bouton_fermer:
 			color = "#960d03"
-		if e.widget == self.bouton_reduire:
+		elif e.widget == self.bouton_reduire:
 			color = "#292626"
+		elif e.widget in self.boutons:
+			color = self.bouton_bg
 		if color != None:
 			e.widget["bg"] = color
 
@@ -82,7 +105,12 @@ def centrer_fenetre(fen):
 	y = (fen.winfo_screenheight() // 2) - (height // 2)
 	fen.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
+def nouvelle_partie(fen, type):
+	print(fen, type)
 
-app = NoBorderWindow()
+def charger_partie(fen, fichier):
+	print(fen, fichier)
+
+app = Fenetre()
 app.little.title("Chesscraft")
 app.mainloop()
